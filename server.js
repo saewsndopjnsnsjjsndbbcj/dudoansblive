47bbcf22c584352e732cac9149d6c4272518f80d
saewsndopjnsnsjjsndbbcj <hauhhdbnhas@gmail.com>
2025-10-22 23:46:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ function getDuDoanVi(tong, duDoan) {
 // === API CHÍNH ===
 app.get('/api/sicbo/vip', async (req, res, next) => {
   try {
-    const response = await axiosInstance.get('https://lssunlive-4.onrender.com/api/sicbo/live');
+    const response = await axiosInstance.get('https://lssunl.onrender.com/api/sicbo/live');
     const latest = response.data;
 
     if (!latest || !latest.Phien) {
@@ -137,4 +137,5 @@ app.listen(PORT, () => {
 });
 
       
+
 
